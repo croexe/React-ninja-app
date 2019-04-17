9b78dd09031d802d01de1ad09c4e32bf53aed9df
irinejs <irinejs@gmail.com>
2019-04-17 15:18:55 +0200

    Conditional output

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -1,24 +1,27 @@
 import React from 'react';
 
-const Ninjas = (props) => {
-    
-        const { ninjas } = props;
-        const ninjaList = ninjas.map(ninja => {
+const Ninjas = ({ ninjas }) => {
+
+    const ninjaList = ninjas.map(ninja => {
+        if (ninja.age > 25) {
             return (
                 <div className="ninja" key={ninja.id}>
-                    <div>Name: { ninja.name}</div>
+                    <div>Name: {ninja.name}</div>
                     <div>Surname: {ninja.surName}</div>
                     <div>Age: {ninja.age}</div>
                     <div>Belt: {ninja.belt}</div>
                 </div>
             )
-        })
-        return(
-            <div className="ninja-list">
-                { ninjaList }
-            </div>
-        )
-    
+        } else {
+            return null;
+        }
+    })
+    return (
+        <div className="ninja-list">
+            {ninjaList}
+        </div>
+    )
+
 }
 
 export default Ninjas
